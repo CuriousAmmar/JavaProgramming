0a86cf01ca7851ed660b79d8269c487f848a1a4f
Ammar <eammar63@gmail.com>
2025-11-01 12:57:24 +0100

    .

---
+++ src/ObjectInfo/Car.java
@@ -0,0 +1,12 @@
+package ObjectInfo;
+
+public class Car {
+    private String model;
+    private String brand;
+    private int year;
+    private double price;
+    private String color;
+    private int quantity;
+    public Car(String model, String brand, int year, double price, String color, int quantity) {}
+
+}
